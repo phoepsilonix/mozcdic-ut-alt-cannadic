a7b59245e6d967867efaf230266e1248502f3eef
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2023-08-16 23:35:52 +0900

    xz -> tar.xz

--- scripts/make.sh
+++ scripts/make.sh
@@ -7,7 +7,13 @@ tar xf ../$SRCTXT.tar.bz2
 
 ruby user_dict.rb -i user_dic_id.def -f ${SRCTXT} > ../${USERDIC}
 
-[[ -e ../${USERDIC}.xz ]] && rm ../${USERDIC}.xz
-xz -9 ../${USERDIC}
+split -d -l 1000000 --additional-suffix=.txt ../$USERDIC $USERDIC-
+rm ../$USERDIC
 
-rm $SRCTXT
+[[ -e ../${USERDIC}.tar.xz ]] && rm ../${USERDIC}*.xz
+
+tar cf ../${USERDIC}.tar ${USERDIC}-*.txt
+xz -9 ../${USERDIC}.tar
+
+rm $SRCTXT $USERDIC-*.txt
+
