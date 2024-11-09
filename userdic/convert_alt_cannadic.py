a709d9258a89a5a2876c17d2d372733a6d0153e4
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-11-09 16:24:46 +0900

    convert alt cannadic(Mozc User Dictionary)
    added hinshi hantei

---
+++ userdic/convert_alt_cannadic.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Author: Masato TOYOSHIMA (phoepsilonix at gmail dot com)
+# Author of based source: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
+# License: Apache License, Version 2.0
+
+import re
+import tarfile
+import urllib.request
+from unicodedata import normalize
+
+from hinshi import hinshi_hantei, hinshi_f, hinshi_t
+
+urllib.request.urlretrieve(
+        'https://ftp.iij.ad.jp/pub/osdn.jp/alt-cannadic/50881/' +
+        'alt-cannadic-110208.tar.bz2', 'alt-cannadic-110208.tar.bz2')
+
+with tarfile.open('alt-cannadic-110208.tar.bz2') as tar:
+    file = tar.extractfile('alt-cannadic-110208/gcanna.ctd')
+    lines = file.read().decode('euc_jp')
+    file = tar.extractfile('alt-cannadic-110208/g_fname.ctd')
+    lines += file.read().decode('euc_jp')
+
+lines = lines.splitlines()
+
+l2 = []
+
+for line in lines:
+    line = line.split(' ')
+
+    # あきびん #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
+    yomi = line[0]
+    yomi = yomi.replace('う゛', 'ゔ')
+
+    # 読みがひらがな以外を含む場合はスキップ
+    # 読みが2文字以下の場合はスキップ
+    if yomi != ''.join(re.findall('[ぁ-ゔー]', yomi)) or \
+            len(yomi) < 3:
+        continue
+
+    hinsi = ''
+    cost_anthy = 0
+
+    # 読みを除去したエントリを作る
+    line = line[1:]
+
+    for c in range(len(line)):
+        # 「#」で始まるエントリの場合は品詞とコストを取得
+        #     #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
+        if line[c][0] == '#':
+            entry = line[c].split('*')
+            hinsi = entry[0]
+            cost_anthy = int(entry[1])
+            continue
+
+        hyouki = normalize('NFKC', line[c])
+
+        # 表記が1文字以下の場合はスキップ
+        if len(hyouki) < 2:
+            continue
+
+        # alt-cannadic のコストから Mozc 辞書のコストを作る
+        # 「#T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン」の場合、
+        # 「空き瓶 空瓶 空きビン 空ビン」の順に優先されるようにする
+        # Mozc 辞書のコストは 8000 台にする
+        cost_mozc = (9000 - cost_anthy) + c
+
+        # 収録する品詞を選択
+        if hinsi.startswith('#T3') or \
+                hinsi.startswith('#T0') or \
+                hinsi.startswith('#JN') or \
+                hinsi.startswith('#KK') or \
+                hinsi.startswith('#CN'):
+            hinshi = hinshi_hantei(hinsi)
+            entry = [yomi, hyouki, str(cost_mozc), hinshi]
+            l2.append(entry)
+
+lines = sorted(l2)
+l2 = []
+
+for i in range(len(lines)):
+    # 読みと表記が前のエントリと同じ場合はスキップ
+    #     [yomi, hyouki, str(cost_mozc)]
+    if lines[i][:2] == lines[i - 1][:2]:
+        continue
+
+    # Mozc 辞書の並びに変更
+    entry = [lines[i][0], lines[i][1], lines[i][3], ""]
+    l2.append('\t'.join(entry) + '\n')
+
+lines = l2
+dict_name = 'user_dic-ut-alt-cannadic'
+
+with open(dict_name, 'w', encoding='utf-8') as file:
+    file.writelines(lines)
--- userdic/mkdict.sh
+++ userdic/mkdict.sh
@@ -5,25 +5,28 @@ DIC=alt-cannadic
 SYSTEMDIC=mozcdic-ut-$DIC
 USERDIC=user_dic-ut-$DIC
 BASE=$PWD
-git clone --filter=tree:0 https://github.com/utuhiro78/merge-ut-dictionaries.git
-(cd merge-ut-dictionaries/src/$DIC/;bash make.sh)
-mv ./merge-ut-dictionaries/src/merge/*.bz2 $BASE/
-tar xf ./$SYSTEMDIC.txt.tar.bz2
+git clone --filter=tree:0 https://github.com/phoepsilonix/IME-User-Dictionary-Mutual-Conversion.git userdic
+(cd userdic; make; mv hinshi.py ../)
+python convert_alt_cannadic.py
 
-curl -LO https://github.com/phoepsilonix/dict-to-mozc/releases/download/v0.3.1/dict-to-mozc-x86_64-unknown-linux-gnu.tar.gz
+curl -LO https://github.com/phoepsilonix/dict-to-mozc/releases/download/v0.4.0/dict-to-mozc-x86_64-unknown-linux-gnu.tar.gz
 tar xf dict-to-mozc-x86_64-unknown-linux-gnu.tar.gz --strip-component=1
 
 curl -LO https://github.com/google/mozc/raw/refs/heads/master/src/data/dictionary_oss/id.def
 # ut dic
 PATH=$HOME/.cargo/bin:$PATH
-./dict-to-mozc -u -U -S -p -i ./id.def -f ./$SYSTEMDIC.txt > ./$USERDIC
+./dict-to-mozc -M -S -p -i ./id.def -f ./$USERDIC > ./$SYSTEMDIC.txt
+tar cf ../release/${SYSTEMDIC}.tar ${SYSTEMDIC}.txt ../LICENSE
+bzip2 -9  ../release/${SYSTEMDIC}.tar
+
+#./dict-to-mozc -M -U -S -p -i ./id.def -f ./$SYSTEMDIC.txt > ./$USERDIC
 ls -la $USERDIC*
 split --numeric-suffixes=1 -l 1000000 --additional-suffix=.txt $USERDIC $USERDIC-
 
 mkdir -p ../release
 [[ -e ../release/${USERDIC}.tar.xz ]] && rm ../release/${USERDIC}.tar.xz
 
-mv ../${SYSTEMDIC}.txt.tar.bz2 ../release/
+#mv ../${SYSTEMDIC}.txt.tar.bz2 ../release/
 tar cf ../release/${USERDIC}.tar ${USERDIC}-*.txt ../LICENSE.user_dic
 xz -9 -e ../release/${USERDIC}.tar
 
