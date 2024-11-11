eea7753206f248e3fdee45b54d2a17d4964a1fc3
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-11-11 15:31:03 +0900

    dict-to-mozc v0.5.0

--- userdic/mkdict.sh
+++ userdic/mkdict.sh
@@ -9,7 +9,7 @@ git clone --filter=tree:0 https://github.com/phoepsilonix/IME-User-Dictionary-Mu
 (cd userdic; make; mv hinshi.py ../)
 python convert_alt_cannadic.py
 
-curl -LO https://github.com/phoepsilonix/dict-to-mozc/releases/download/v0.4.6/dict-to-mozc-x86_64-unknown-linux-gnu.tar.gz
+curl -LO https://github.com/phoepsilonix/dict-to-mozc/releases/download/v0.5.0/dict-to-mozc-x86_64-unknown-linux-gnu.tar.gz
 tar xf dict-to-mozc-x86_64-unknown-linux-gnu.tar.gz --strip-component=1
 
 curl -LO https://github.com/google/mozc/raw/refs/heads/master/src/data/dictionary_oss/id.def
